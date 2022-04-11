f2d471b84afc3a6aa31dd99470d86f4e0e34cda2
hazman <muhammad.hazman.shahul.hameed@intel.com>
2022-04-11 13:02:13 +0800

    update on rename_folder.py
    uploaded the wrong version of file, this is the latest

--- data/GTZAN/rename_folder.py
+++ data/GTZAN/rename_folder.py
@@ -4,63 +4,88 @@ from tqdm import tqdm
 def rename_folder():
     os.chdir('genres')
     genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
-    distortions = 'gaps hums discontinuity disco hiphop jazz metal pop reggae rock'.split()
+    distortions = 'randomsilence click_n_pop discontinuity hum white_noise2 hum2 oversampling undersampling saturated randomsilence5'.split()
     for idx,genre in tqdm(enumerate(genres),total=len(genres)):
         if genre == 'blues': 
             target_main = genre
             target_dest = distortions[idx]
-            for count,fname in enumerate(os.listdir(genre)):
-                
+            for count,fname in enumerate(os.listdir(genre)):   
                 targ = target_main+'/'+fname
                 dest = f_name=target_dest+'_'+str(count)+'.wav'
                 os.rename(targ, genre+'/'+dest)
             os.rename(target_main, target_dest)
-                
         if genre == 'classical':
+            target_main = genre
+            target_dest = distortions[idx]
             for count,fname in enumerate(os.listdir(genre)):
                 targ = genre+'/'+fname
                 dest = f_name=distortions[idx]+'_'+str(count)+'.wav'
-                os.rename(targ, genre+'/'+dest)            
+                os.rename(targ, genre+'/'+dest) 
+            os.rename(target_main, target_dest)
         if genre == 'country':
+            target_main = genre
+            target_dest = distortions[idx]
             for count,fname in enumerate(os.listdir(genre)):
                 targ = genre+'/'+fname
                 dest = f_name=distortions[idx]+'_'+str(count)+'.wav'
-                os.rename(targ, genre+'/'+dest)            
+                os.rename(targ, genre+'/'+dest)  
+            os.rename(target_main, target_dest)
         if genre == 'disco':
+            target_main = genre
+            target_dest = distortions[idx]
             for count,fname in enumerate(os.listdir(genre)):
                 targ = genre+'/'+fname
                 dest = f_name=distortions[idx]+'_'+str(count)+'.wav'
-                os.rename(targ, genre+'/'+dest)           
+                os.rename(targ, genre+'/'+dest)
+            os.rename(target_main, target_dest)
         if genre == 'hiphop':
+            target_main = genre
+            target_dest = distortions[idx]
             for count,fname in enumerate(os.listdir(genre)):
                 targ = genre+'/'+fname
                 dest = f_name=distortions[idx]+'_'+str(count)+'.wav'
-                os.rename(targ, genre+'/'+dest)           
+                os.rename(targ, genre+'/'+dest)
+            os.rename(target_main, target_dest)
         if genre == 'jazz':
+            target_main = genre
+            target_dest = distortions[idx]
             for count,fname in enumerate(os.listdir(genre)):
                 targ = genre+'/'+fname
                 dest = f_name=distortions[idx]+'_'+str(count)+'.wav'
-                os.rename(targ, genre+'/'+dest)            
+                os.rename(targ, genre+'/'+dest) 
+            os.rename(target_main, target_dest)
         if genre == 'metal':
+            target_main = genre
+            target_dest = distortions[idx]
             for count,fname in enumerate(os.listdir(genre)):
                 targ = genre+'/'+fname
                 dest = f_name=distortions[idx]+'_'+str(count)+'.wav'
-                os.rename(targ, genre+'/'+dest)           
+                os.rename(targ, genre+'/'+dest)
+            os.rename(target_main, target_dest)
         if genre == 'pop':
+            target_main = genre
+            target_dest = distortions[idx]
             for count,fname in enumerate(os.listdir(genre)):
                 targ = genre+'/'+fname
                 dest = f_name=distortions[idx]+'_'+str(count)+'.wav'
-                os.rename(targ, genre+'/'+dest)            
+                os.rename(targ, genre+'/'+dest)
+            os.rename(target_main, target_dest)
         if genre == 'reggae':
+            target_main = genre
+            target_dest = distortions[idx]
             for count,fname in enumerate(os.listdir(genre)):
                 targ = genre+'/'+fname
                 dest = f_name=distortions[idx]+'_'+str(count)+'.wav'
-                os.rename(targ, genre+'/'+dest)            
+                os.rename(targ, genre+'/'+dest)
+            os.rename(target_main, target_dest)
         if genre == 'rock':
-             for count,fname in enumerate(os.listdir(genre)):
+            target_main = genre
+            target_dest = distortions[idx]
+            for count,fname in enumerate(os.listdir(genre)):
                 targ = genre+'/'+fname
                 dest = f_name=distortions[idx]+'_'+str(count)+'.wav'
-                os.rename(targ, genre+'/'+dest)           
+                os.rename(targ, genre+'/'+dest) 
+            os.rename(target_main, target_dest)
     os.chdir('..')
 
 if __name__=='__main__':
